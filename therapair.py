dd5032a793c5a5e97e160b94b4b290692af97dbb
EJ Castillo <eugene.j.castillo@gmail.com>
2019-04-13 17:48:14 -0500

    added bootstrap and template

--- therapair.py
+++ therapair.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
-    return "Home page"
+    return render_template("template.html")
 
 if __name__ == '__main__':
 	app.run(debug = True)
