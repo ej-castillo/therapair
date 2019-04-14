4a5dcec9ccf317d27e07dd58252eccd9205bd43c
Tate Song <tlsong54321@gmail.com>
2019-04-14 04:49:01 -0500

    Clean up style

--- therapair.py
+++ therapair.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route("/")
-def Home():
-    return render_template("home.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 @app.route("/therapists")
-def Therapists():
+def therapists():
 	return render_template("therapists.html")
 
 @app.route("/map")
-def Map():
-	user_zip = request.form.get('zip')
+def map():
+	user_zip = request.form.get("zip")
 	return render_template("map.html", zip=user_zip)
 
-if __name__ == '__main__':
-	app.run(debug = True)
+if __name__ == "__main__":
+	app.run(debug=True)
