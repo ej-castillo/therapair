72e0fd097495592e06f23d627c4b32c059b955f5
EJ Castillo <eugene.j.castillo@gmail.com>
2019-04-13 20:53:34 -0500

    migrated to flask-bootstrap

--- therapair.py
+++ therapair.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def Home():
-    return render_template("template.html")
+    return render_template("home.html")
 
 if __name__ == '__main__':
 	app.run(debug = True)
